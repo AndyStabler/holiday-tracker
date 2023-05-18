1e7b78efdaa2820ce0e22e7d010935938456e156
Andy Stabler <me@andystabler.co.uk>
2023-05-18 11:48:37 +0100

    Add devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_secure_password
   belongs_to :organisation
 
   attribute :allowance, default: 30
+  validates :email_address, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # is this needed?
+  devise_for :users
+  root to: 'organisations#new'
   resources :organisations, only: [:new, :create, :show]
 end
